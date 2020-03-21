8650f994db22be4ad5a98bf28a1920eccd87c0ed
kevgorix <dev.kevingomez@gmail.com>
2020-03-20 20:21:45 -0500

    Introducing getSnapshotBeforeUpdate

--- src/components/Users/UsersList.tsx
+++ src/components/Users/UsersList.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
+import { withStyles } from "@material-ui/core/styles";
 
 type UsersProps = {
+  classes?: any;
   users: string[];
   selectedName: string | null;
   selectUser: (name: string) => void;
@@ -12,7 +14,8 @@ type UsersState = {
   sortedNames: string[];
 };
 
-export default class UsersList extends React.Component<UsersProps> {
+class UsersList extends React.Component<UsersProps> {
+  private listRef = React.createRef<HTMLDivElement>();
   state: UsersState = {
     sortedNames: []
   };
@@ -24,24 +27,58 @@ export default class UsersList extends React.Component<UsersProps> {
     };
   }
 
-  render() {
-    const { selectedName, selectUser } = this.props;
-    const { sortedNames } = this.state;
+  getSnapshotBeforeUpdate(prevProps: UsersProps, prevState: UsersState) {
+    if (prevProps.users.length < this.props.users.length) {
+      const list = this.listRef.current;
+      if (list) {
+        console.log(list.scrollHeight - list.scrollTop);
+        return list.scrollHeight - list.scrollTop;
+      }
+    }
+    return null;
+  }
 
+  componentDidUpdate(
+    prevProps: UsersProps,
+    prevState: UsersState,
+    snapshot: number
+  ) {
+    if (snapshot !== null) {
+      const list = this.listRef.current;
+      if (list) {
+        list.scrollTop = list.scrollHeight - snapshot;
+      }
+    }
+  }
+
+  render() {
+    const { selectedName, selectUser, classes } = this.props;
+    const { sortedNames } = this.state;
     return (
-      <List aria-label="users">
-        {sortedNames.map((name: string) => (
-          <ListItem
-            button
-            onClick={() => selectUser(name)}
-            key={name}
-            divider
-            selected={selectedName === name}
-          >
-            {name}
-          </ListItem>
-        ))}
-      </List>
+      <div className={classes.root} ref={this.listRef}>
+        <List aria-label="users">
+          {sortedNames.map((name: string) => (
+            <ListItem
+              button
+              onClick={() => selectUser(name)}
+              key={name}
+              divider
+              selected={selectedName === name}
+            >
+              {name}
+            </ListItem>
+          ))}
+        </List>
+      </div>
     );
   }
 }
+
+const styles = {
+  root: {
+    height: "400px",
+    overflow: "scroll"
+  }
+};
+
+export default withStyles(styles)(UsersList);
